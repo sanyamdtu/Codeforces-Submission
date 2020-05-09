eed2d51104736bb4146764102ebb91b0be0997ba
Sanyam_Srivastava <sanyamsri2001@gmail.com>
2020-05-09 20:33:00 +0530

    Add solution for problem `A - Sum of Round Numbers` Link: https://codeforces.com/contest/1352/problem/A Tags: implementation, math, *800 Ref: https://codeforces.com/contest/1352/submission/79499332

---
+++ codeforces/1352/A.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+//#define forn(i,n) for(int i=0;i<n;i++)
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n,num,in=0;
+        cin>>n;
+        vector<int> v;
+        while(n>0){
+            int r = (n%10);
+            if(r!=0){
+                num = r*pow(10,in);
+                v.push_back(num);
+            }
+            n=n/10;
+            in++;
+        }
+        cout<<v.size()<<endl;
+        for(int i=0;i<v.size();i++)
+            cout<<v[i]<<" ";
+        cout<<endl;
+    }
+}
